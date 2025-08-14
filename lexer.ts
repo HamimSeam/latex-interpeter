4f7f771eace6daa5f44f21316095c211679b21da
Hamim Seam <hamimhseam@gmail.com>
2025-08-14 17:09:23 -0400

    Complete initial lexer draft

--- lexer.ts
+++ lexer.ts
@@ -10,7 +10,8 @@ export enum TokenType {
   RIGHT_PAREN,
   SEPARATOR,
   COMMENT,
-  EOS,
+  EOL,
+  UNKNOWN,
 }
 
 export interface Token {
@@ -22,44 +23,228 @@ enum LexerState {
   INITIAL,
   ESCAPE,
   COMMAND,
+  COMMENT,
+  COMMENT_EXIT,
+  INT,
+  FLOAT,
+  SCIENTIFIC,
 }
 
 const isAlpha = (str: string): boolean => /^[a-zA-Z]+$/.test(str);
+const isNumeric = (str: string): boolean => /^[0-9]+$/.test(str);
 const isWhitespace = (str: string): boolean => /^[ \t\n]+$/.test(str);
 
-export function tokenizeLaTeX(text: string) {
+const operators: Set<string> = new Set([
+  "triangle",
+  "angle",
+  "overline",
+  "odot",
+  "cdot",
+  "times",
+  "div",
+  "+",
+  "-",
+]);
+
+const relations: Set<string> = new Set([
+  "cong",
+  "sim",
+  "gt",
+  "lt",
+  "geq",
+  "leq",
+  "in",
+  "notin",
+  "subset",
+  "subseteq",
+  "supset",
+  "supseteq",
+  "=",
+  ">",
+  "<",
+]);
+
+const delimiters: Set<string> = new Set(["(", ")", "{", "}"]);
+
+function getDelimiterTokenType(c: string): TokenType {
+  let type: TokenType = TokenType.UNKNOWN;
+
+  switch (c) {
+    case "(":
+      type = TokenType.LEFT_PAREN;
+    case ")":
+      type = TokenType.RIGHT_PAREN;
+    case "{":
+      type = TokenType.LEFT_BRACE;
+    case "}":
+      type = TokenType.RIGHT_BRACE;
+    case ",":
+      type = TokenType.SEPARATOR;
+  }
+
+  return type;
+}
+
+function generateToken(type: TokenType, value: string): Token {
+  return {
+    type,
+    value,
+  };
+}
+
+function handleSingleCharToken(
+  tokens: Token[],
+  state: LexerState,
+  c: string
+): LexerState {
+  let newState: LexerState = LexerState.INITIAL;
+  if (c in delimiters) {
+    tokens.push(generateToken(getDelimiterTokenType(c), c));
+  } else if (c in operators) {
+    tokens.push(generateToken(TokenType.OPERATOR, c));
+  } else if (c in relations) {
+    tokens.push(generateToken(TokenType.RELATION, c));
+  } else if (state !== LexerState.COMMAND && isAlpha(c)) {
+    tokens.push(generateToken(TokenType.IDENTIFIER, c));
+  } else if (c === "\\") {
+    newState = LexerState.ESCAPE;
+  }
+  return newState;
+}
+
+export function tokenizeLaTeX(text: string): Token[] {
   const tokens: Token[] = [];
-  let state = LexerState.INITIAL;
-  let buffer = "";
+  let state: LexerState = LexerState.INITIAL;
+  let buffer: string = "";
 
   for (const c of text) {
     switch (state) {
       case LexerState.INITIAL:
-        if (c === "\\") state = LexerState.ESCAPE;
+        if (c === "%") {
+          state = LexerState.COMMENT;
+        } else if (c === "\\") {
+          state = LexerState.ESCAPE;
+        } else if (isNumeric(c)) {
+          state = LexerState.INT;
+          buffer += c;
+        } else {
+          state = handleSingleCharToken(tokens, state, c);
+        }
+        break;
+
+      case LexerState.COMMENT:
+        if (c == "\\") state = LexerState.COMMENT_EXIT;
+        else buffer += c;
+        break;
+
+      case LexerState.COMMENT_EXIT:
+        if (c === "\\") {
+          tokens.push(generateToken(TokenType.COMMENT, buffer));
+          state = LexerState.INITIAL;
+          buffer = "";
+        } else {
+          buffer += c;
+          state = LexerState.COMMENT;
+        }
         break;
 
       case LexerState.ESCAPE:
-        if (c === "\\") {
-          tokens.push({
-            type: TokenType.EOS,
-            value: "\\\\",
-          });
-        } else if (isAlpha(c)) {
+        if (isAlpha(c)) {
+          buffer += c;
           state = LexerState.COMMAND;
-          buffer = c;
+        } else if (c === "\\") {
+          tokens.push(generateToken(TokenType.EOL, "\\\\"));
+          state = LexerState.INITIAL;
+          buffer = "";
+        } else {
+          tokens.push(generateToken(TokenType.UNKNOWN, c));
+          state = LexerState.INITIAL;
+          buffer = "";
         }
         break;
 
       case LexerState.COMMAND:
-        if (isAlpha(c)) buffer += c;
-        else if (isWhitespace(c)) {
-          state = LexerState.INITIAL;
-          tokens.push({
-            type: TokenType.COMMAND,
-            value: buffer,
-          });
+        if (isAlpha(c)) {
+          buffer += c;
+        } else {
+          if (buffer in operators) {
+            tokens.push(generateToken(TokenType.OPERATOR, buffer));
+          } else if (buffer in relations) {
+            tokens.push(generateToken(TokenType.RELATION, buffer));
+          } else {
+            tokens.push(generateToken(TokenType.COMMAND, buffer));
+          }
+
+          state = handleSingleCharToken(tokens, state, c);
+          buffer = "";
         }
         break;
+
+      case LexerState.INT:
+        if (isNumeric(c)) {
+          buffer += c;
+        } else if (c === ".") {
+          buffer += c;
+          state = LexerState.FLOAT;
+        } else if (c === "e") {
+          buffer += c;
+          state = LexerState.SCIENTIFIC;
+        } else {
+          tokens.push(generateToken(TokenType.NUMBER, buffer));
+
+          state = handleSingleCharToken(tokens, state, c);
+          buffer = "";
+        }
+        break;
+
+      case LexerState.FLOAT:
+        if (isNumeric(c)) {
+          buffer += c;
+        } else {
+          tokens.push(generateToken(TokenType.NUMBER, buffer));
+
+          state = handleSingleCharToken(tokens, state, c);
+          buffer = "";
+        }
+        break;
+
+      case LexerState.FLOAT:
+        if (isNumeric(c)) {
+          buffer += c;
+        } else {
+          tokens.push(generateToken(TokenType.NUMBER, buffer));
+
+          state = handleSingleCharToken(tokens, state, c);
+          buffer = "";
+        }
+        break;
+    }
+  }
+
+  if (buffer.length > 0) {
+    const numberStates: LexerState[] = [
+      LexerState.INT,
+      LexerState.FLOAT,
+      LexerState.SCIENTIFIC,
+    ];
+
+    const commentStates: LexerState[] = [
+      LexerState.COMMENT,
+      LexerState.COMMENT_EXIT,
+    ];
+
+    if (state === LexerState.COMMAND) {
+      if (buffer in operators) {
+        tokens.push(generateToken(TokenType.OPERATOR, buffer));
+      } else if (buffer in relations) {
+        tokens.push(generateToken(TokenType.RELATION, buffer));
+      } else {
+        tokens.push(generateToken(TokenType.COMMAND, buffer));
+      }
+    } else if (numberStates.includes(state)) {
+      tokens.push(generateToken(TokenType.NUMBER, buffer));
+    } else if (commentStates.includes(state)) {
+      tokens.push(generateToken(TokenType.COMMENT, buffer));
     }
   }
 
--- tokenizeLaTeX.test.ts
+++ tokenizeLaTeX.test.ts
@@ -4,7 +4,40 @@ import { tokenizeLaTeX, TokenType } from "./lexer"; // adjust path
 describe("tokenizeLaTeX", () => {
   it("recognizes commands", () => {
     expect(tokenizeLaTeX("\\triangle")).toEqual([
-      { type: TokenType.COMMAND, value: "triangle" }
+      { type: TokenType.COMMAND, value: "triangle" },
+    ]);
+  });
+
+  it("recognizes newlines", () => {
+    expect(tokenizeLaTeX("\\\\")).toEqual([
+      { type: TokenType.EOL, value: "\\\\" },
+    ]);
+  });
+
+  it("recognizes comments", () => {
+    expect(tokenizeLaTeX("%This is a comment")).toEqual([
+      { type: TokenType.COMMENT, value: "This is a comment" },
+    ]);
+  });
+
+  it("recognizes end of comments", () => {
+    expect(tokenizeLaTeX("%This is a comment until here\\\\A")).toEqual([
+      { type: TokenType.COMMENT, value: "This is a comment until here" },
+      { type: TokenType.IDENTIFIER, value: "A" },
+    ]);
+  });
+
+  it("recognizes end of comments with continuation", () => {
+    expect(tokenizeLaTeX("%This is a comment until here\\\\A")).toEqual([
+      { type: TokenType.COMMENT, value: "This is a comment until here" },
+      { type: TokenType.IDENTIFIER, value: "A" },
+    ]);
+  });
+
+  it("recognizes inline comments", () => {
+    expect(tokenizeLaTeX("\\triangle %This is a comment")).toEqual([
+      { type: TokenType.COMMAND, value: "triangle" },
+      { type: TokenType.COMMENT, value: "This is a comment" },
     ]);
   });
 });
